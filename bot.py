7e324d70a54fbfae25f314a74eed5ca99d681768
lavos <76091914+lavosAndre@users.noreply.github.com>
2023-12-30 20:55:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,39 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-import time
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetHistory, ForwardMessages
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-TOKEN = '6876062883:AAFBonyQGtK_DmnQyEcnE1g40tLTF-Tn3K4'
-
-# Replace 'SOURCE_CHANNEL_ID' and 'DESTINATION_CHANNEL_ID' with your actual channel IDs
+# Replace 'API_ID', 'API_HASH', 'SOURCE_CHANNEL_ID', and 'DESTINATION_CHANNEL_ID' with your actual values
+API_ID = '25618507'
+API_HASH = 'b8b91983b578360ec05a7e88e17b06ea'
 SOURCE_CHANNEL_ID = -1001666241790
 DESTINATION_CHANNEL_ID = -1002117307978
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Bot is running.')
-
-def move_messages(context: CallbackContext) -> None:
+def move_messages(client):
     # Fetch messages in batches of 100
-    messages = context.bot.get_chat_history(chat_id=SOURCE_CHANNEL_ID, limit=100)
+    messages = client.invoke(GetHistory(SOURCE_CHANNEL_ID, 0, 0, 100, 0, 0, 0)).messages
 
     # Forward each message to the destination channel
     for message in messages:
-        context.bot.forward_message(chat_id=DESTINATION_CHANNEL_ID, from_chat_id=SOURCE_CHANNEL_ID, message_id=message.message_id)
+        client.invoke(ForwardMessages(DESTINATION_CHANNEL_ID, [message.id]))
 
     # If there are more messages, schedule the next batch
     if messages and len(messages) == 100:
-        last_message_id = messages[-1].message_id
-        context.job_queue.run_once(lambda c: move_messages(c), when=2)  # Wait 2 seconds before the next batch
+        move_messages(client)
 
-def main() -> None:
-    updater = Updater(TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+def main():
+    # Create a Telethon client
+    client = TelegramClient('1BVtsOK4Bu3yCEz9R9Uo6XBM4sWqYpi2o0HpvRmLS_UkH7lkaIlwx-bDHrYaYj11Cii5_QkZXuBgutr2FIomkVxgmDQLPihse8_PnsrAR28XTCKn21WXSpmwrOdlWMa_z9ehGQUnGvuRQvMKEd4-fH4Lb_iuw6WadAwMenB5tGv1iY8sSK7acBGlOMbZmW5ORpEG3TtHWtDSq4rNxKvH9NIs1YikjBuRn5SeBDRPDApTiE_Srwq5nEGfX00-51J4UUX-seqrs_0lP1oDjFuU7QryeVL5XkRg7saAh5UWctcTvSSY9EI-KbD1A0MWjz6z8FF5BkxbHpHjmsHvG0vZkYUWQYfTnnag=', API_ID, API_HASH)
 
-    # Add command handlers
-    dispatcher.add_handler(CommandHandler("start", start))
+    # Connect to the Telegram server
+    client.connect()
 
-    # Schedule the initial move_messages call
-    updater.job_queue.run_once(lambda c: move_messages(c), when=0)
+    # Log in (you don't need to provide a phone number for a bot)
+    client.sign_in(bot_token='6876062883:AAFBonyQGtK_DmnQyEcnE1g40tLTF-Tn3K4')
 
-    # Start the Bot
-    updater.start_polling()
+    # Fetch and forward messages
+    move_messages(client)
 
-    # Run the bot until you send a signal to stop it
-    updater.idle()
+    # Disconnect the client
+    client.disconnect()
 
 if __name__ == '__main__':
     main()
