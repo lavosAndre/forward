dd11be67cafdb765dcc8048c93a1c6d0e5202eb3
lavos <76091914+lavosAndre@users.noreply.github.com>
2023-12-30 20:25:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,9 +12,9 @@ DESTINATION_CHANNEL_ID = -1002117307978
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Bot is running.')
 
-def move_messages(context: CallbackContext, last_message_id=None) -> None:
+def move_messages(context: CallbackContext) -> None:
     # Fetch messages in batches of 100
-    messages = context.bot.get_chat_history(chat_id=SOURCE_CHANNEL_ID, limit=100, offset_id=last_message_id)
+    messages = context.bot.get_chat_history(chat_id=SOURCE_CHANNEL_ID, limit=100)
 
     # Forward each message to the destination channel
     for message in messages:
@@ -23,17 +23,17 @@ def move_messages(context: CallbackContext, last_message_id=None) -> None:
     # If there are more messages, schedule the next batch
     if messages and len(messages) == 100:
         last_message_id = messages[-1].message_id
-        context.job_queue.run_once(lambda c: move_messages(c, last_message_id), when=2)  # Wait 2 seconds before the next batch
+        context.job_queue.run_once(lambda c: move_messages(c), when=2)  # Wait 2 seconds before the next batch
 
 def main() -> None:
-    updater = Updater(TOKEN)
+    updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
     # Add command handlers
     dispatcher.add_handler(CommandHandler("start", start))
 
     # Schedule the initial move_messages call
-    updater.job_queue.run_once(move_messages, when=0)
+    updater.job_queue.run_once(lambda c: move_messages(c), when=0)
 
     # Start the Bot
     updater.start_polling()
